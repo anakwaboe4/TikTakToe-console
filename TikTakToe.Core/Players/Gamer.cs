0e3952b3f2e3534bb3681f186140b26eab398ad9
Ewout Hebbrecht <ewout.hebbrecht@student@student.odisee.be>
2022-12-18 21:13:55 +0100

    rebased changesremoved unessecary files & added code for ai

--- TikTakToe.Core/Players/Gamer.cs
+++ TikTakToe.Core/Players/Gamer.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TikTakToe.Core.Boards;
 
 namespace TikTakToe.Core.Players {
     public class Gamer : Player {
+        public override int CalculateBestMove(Board board) {
+            throw new NotImplementedException();
+        }
 
+        public override int CalculateNextMove(Board board) {
+            throw new NotImplementedException();
+        }
     }
 }
--- TikTakToe.Core/Players/Player.cs
+++ TikTakToe.Core/Players/Player.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TikTakToe.Core.Boards;
 
 namespace TikTakToe.Core.Players {
     public abstract class Player {
         public bool IsAI { get; }
+
+        public abstract int CalculateNextMove(Board board);
+
+        public abstract int CalculateBestMove(Board board);
     }
 }
